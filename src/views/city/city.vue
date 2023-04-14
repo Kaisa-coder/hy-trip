9d4b0714177cbc031b2280809e536be586574120
zpf <2384085701@qq.com>
2023-04-15 07:57:48 +0800

    新增city页面

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,13 +1,30 @@
 <script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { getCityAll } from '@/service/index'
 
+const searchValue = ref('')
+const router = useRouter()
+
+const cancelClick = () => {
+  router.back()
+}
+
+const tabActive = ref(0)
+
+getCityAll().then(res => {
+  console.log(res)
+  const { data } = res
+})
 </script>
 
 <template>
   <div class="city top-page">
-    <h2>city</h2>
-    <template v-for="item in 100" :key="item">
-      <div>hhh1{{ item }}</div>
-    </template>
+    <van-search v-model="searchValue" placeholder="城市/区域位置" shape="round" show-action @cancel="cancelClick" />
+    <van-tabs v-model:active="tabActive" color="#ff9854">
+      <van-tab title="国内·港澳台"></van-tab>
+      <van-tab title="海外"></van-tab>
+    </van-tabs>
   </div>
 </template>
 
